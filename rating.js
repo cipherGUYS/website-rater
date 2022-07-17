4d2ecede84d2c2c2d980ca707baf52ebd56f70e4
Akash-Vijay <akashvijay789@gmail.com>
2022-07-17 19:36:09 +0530

    [Updated] Student Data

--- rating.js
+++ rating.js
@@ -16,24 +16,14 @@ function fill_temp() {
             url: "https://akashvijay.live/"
         },
         {
-            name: "Vivekanand R",
-            frameurl: "https://vivek-unni.github.io/Portfolio1stTry/",
-            url: "https://vivek-unni.github.io/Portfolio1stTry/"
+            name: "Mathews P Mathew",
+            frameurl: "https://mathewspmathew.github.io/mathewspmathew/",
+            url: "https://mathewspmathew.github.io/mathewspmathew/"
         },
         {
-            name: "Bibin Biju",
-            frameurl: "https://bibinbiju111.github.io/Portfolio_Demo/",
-            url: "https://bibinbiju111.github.io/Portfolio_Demo/"
-        },
-        {
-            name: "Jimmy Jose",
-            frameurl: "https://jimmyj13.github.io/Jimmy-portfolio/",
-            url: "https://jimmyj13.github.io/Jimmy-portfolio/"
-        },
-        {
-            name: "Nikhil Jose",
-            frameurl: "https://nikhiljose-123.github.io/Portfolio/",
-            url: "https://nikhiljose-123.github.io/Portfolio/"
+            name: "Reenphy George",
+            frameurl: "https://reenphygeorge.github.io/portfolio/",
+            url: "https://reenphygeorge.github.io/portfolio/"
         }
         ]
     }
