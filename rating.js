06555dc245f41ddc6448e70500f6d81caf547f78
Akash-Vijay <akashvijay789@gmail.com>
2022-07-17 19:28:11 +0530

    [Added] Student Data

--- rating.js
+++ rating.js
@@ -4,15 +4,36 @@ function fill_temp() {
             name: "cipherGUYS",
             frameurl: "https://cipherguys.github.io",
             url: "https://cipherguys.github.io"
-        }, {
-            name: "Handlebars",
-            frameurl: "https://handlebarsjs.com/",
-            url: "https://handlebarsjs.com/"
         },
         {
-            name:"Tomin",
-            frameurl:"https://tomin-joy.github.io",
-            url:"https://tomin-joy.github.io"
+            name: "Tomin Joy",
+            frameurl: "https://tomin-joy.github.io",
+            url: "https://tomin-joy.github.io"
+        },
+        {
+            name: "Akash Vijay",
+            frameurl: "https://akashvijay.live/",
+            url: "https://akashvijay.live/"
+        },
+        {
+            name: "Vivekanand R",
+            frameurl: "https://vivek-unni.github.io/Portfolio1stTry/",
+            url: "https://vivek-unni.github.io/Portfolio1stTry/"
+        },
+        {
+            name: "Bibin Biju",
+            frameurl: "https://bibinbiju111.github.io/Portfolio_Demo/",
+            url: "https://bibinbiju111.github.io/Portfolio_Demo/"
+        },
+        {
+            name: "Jimmy Jose",
+            frameurl: "https://jimmyj13.github.io/Jimmy-portfolio/",
+            url: "https://jimmyj13.github.io/Jimmy-portfolio/"
+        },
+        {
+            name: "Nikhil Jose",
+            frameurl: "https://nikhiljose-123.github.io/Portfolio/",
+            url: "https://nikhiljose-123.github.io/Portfolio/"
         }
         ]
     }
