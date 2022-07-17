17f98084c85c7b4ee30529ce3846dd25da203c84
reenphygeorge <reenphygeorge@gmail.com>
2022-07-17 18:22:28 +0530

    chore: handlebars added

---
+++ rating.js
@@ -0,0 +1,17 @@
+function fill_temp() {
+    var data = {
+        list: [{
+            name: "cipherGUYS",
+            frameurl: "https://cipherguys.github.io",
+            url: "https://cipherguys.github.io"
+        }, {
+            name: "Handlebars",
+            frameurl: "https://handlebarsjs.com/",
+            url: "https://handlebarsjs.com/"
+        }
+        ]
+    }
+    var template = Handlebars.compile(document.querySelector("#template").innerHTML);
+    var filled = template(data);
+    document.querySelector("#output").innerHTML = filled;
+}
